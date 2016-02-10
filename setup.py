6b5e3bf33473a20d7de887f5e1466cf6c748c417
Safia Abdalla <safia@safia.rocks>
2016-02-10 15:58:02 -0600

    Initial project setup

---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+try:
+    long_description = open("README.md").read()
+except IOError:
+    long_description = ""
+
+setup(
+    name="zubat",
+    version="0.1.0",
+    description="An implementation of logistic regression in Python for educational purposes ",
+    license="MIT",
+    author="Safia Abdalla",
+    packages=find_packages(),
+    install_requires=[],
+    long_description=long_description,
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.5",
+    ]
+)
